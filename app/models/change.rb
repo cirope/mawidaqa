aa17985e715c00eb3aa68da7732c368128704a13
Franco Catena <francocatena@gmail.com>
2012-05-14 14:46:33 -0300

    Changed sort order in changes

--- app/models/change.rb
+++ app/models/change.rb
@@ -3,6 +3,9 @@ class Change < ActiveRecord::Base
   
   attr_accessible :content, :made_at, :document_id, :lock_version
   
+  # Scopes
+  default_scope order("#{table_name}.made_at DESC")
+  
   # Restrictions
   validates :content, :made_at, presence: true
   validates :made_at, timeliness: { type: :date }, allow_nil: true,
