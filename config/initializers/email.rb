047e9c199cb5180212f74b574062ade65988ca6e
Franco Catena <francocatena@gmail.com>
2012-12-26 11:40:56 -0300

    Added admin subdomain to email host...

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,4 +1,6 @@
 MawidaQA::Application.configure do
-  config.action_mailer.default_url_options = { host: APP_CONFIG['public_host'] }
+  config.action_mailer.default_url_options = {
+    host: ['www', APP_CONFIG['public_host']].join('.')
+  }
   config.action_mailer.smtp_settings = APP_CONFIG['smtp'].symbolize_keys
 end
