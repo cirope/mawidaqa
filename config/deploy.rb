346003b31ca1625ab8353cc86d7fd686533d167f
Franco Catena <francocatena@gmail.com>
2013-12-06 15:02:06 -0300

    Repository moved to Cirope's

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, 'mawidaqa.com'
 set :user, 'deployer'
-set :repo_url, 'git://github.com/francocatena/mawidaqa.git'
+set :repo_url, 'git://github.com/cirope/mawidaqa.git'
 
 set :format, :pretty
 set :log_level, :info
