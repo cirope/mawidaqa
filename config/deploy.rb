aa33066b92e8202f7a8a511fb363a037226b09a1
Franco Catena <francocatena@gmail.com>
2013-12-08 10:42:22 -0300

    Use unicorn upgrade instead of touch restart.txt

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      execute 'service unicorn upgrade'
     end
   end
 
