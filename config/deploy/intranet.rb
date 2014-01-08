dab93e14b56ca294d0915ea0cad9d4fe1325d646
Franco Catena <francocatena@gmail.com>
2014-01-08 13:46:17 -0300

    Real intranet servers addresses

--- config/deploy/intranet.rb
+++ config/deploy/intranet.rb
@@ -3,5 +3,8 @@ set :rails_env, 'production'
 set :branch, 'intranet'
 
 role :all, %w{10.241.161.243}
+# role :web, %w{10.1.74.146}
+# role :app, %w{10.241.161.243}
+# role :db,  %w{10.241.161.240}
 
 server '10.241.161.243', user: 'deployer', roles: %w{web app db}
